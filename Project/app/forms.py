41c00b68a9f689a58d6596749bafbcd17cbbb531
HenrySmith22227188 <49471409+HenrySmith22227188@users.noreply.github.com>
2019-05-19 18:46:24 +0800

    Combined Compare-Top-Ranks and Pre-Merge
    Manual Merge

--- Project/app/forms.py
+++ Project/app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, TextAreaField
 from wtforms.validators import DataRequired, regexp, EqualTo, Email, ValidationError, Length
 from app.models import User
 import requests
@@ -28,6 +28,22 @@ class CreateAccountForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+
+class UpdateUsernameForm(FlaskForm):
+    username = StringField('Change Username', validators=[DataRequired(), Length(min=2, max=30)])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+
+class FeedbackForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    feedback = TextAreaField('Feedback', validators=[DataRequired()])
+    submit = SubmitField('Submit Feedback')
 
 
 class CompareForm(FlaskForm):
--- Project/app/routes.py
+++ Project/app/routes.py
@@ -1,11 +1,13 @@
 from flask import render_template, flash, redirect, url_for, request
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 import requests
+from sqlalchemy import desc
+
 
 from app import app, db
-from app.forms import LoginForm, CreateAccountForm, CompareForm
-from app.models import User
+from app.forms import LoginForm, CreateAccountForm, CompareForm, UpdateUsernameForm, FeedbackForm
+from app.models import User, Comparison, Feedback
 
 
 @app.route("/")
@@ -58,12 +60,34 @@ def topRanks():
 
 @app.route("/feedback")
 def feedback():
-    return render_template("feedback.html", title='Feedback')
+    feedback = Feedback.query.order_by(desc(Feedback.timestamp)).limit(25).all()
+    return render_template("feedback.html", title='Feedback', feedback=feedback)
 
 
-@app.route("/account")
+@app.route("/feedback/new", methods=['GET', 'POST'])
+@login_required
+def new_feedback():
+    form = FeedbackForm()
+    if form.validate_on_submit():
+        feedback = Feedback(user=current_user, uid=current_user.id, title=form.title.data, text=form.feedback.data)
+        db.session.add(feedback)
+        db.session.commit()
+        flash('Thank you for four feedback.')
+        return redirect(url_for('feedback'))
+    return render_template('new_feedback.html', title='Your Feedback', form=form)
+
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
 def account():
-    return render_template("account.html", title='Account')
+    form = UpdateUsernameForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        db.session.commit()
+        flash('Username changed.')
+        return redirect(url_for('account'))
+    feedback = Feedback.query.filter_by(user=current_user).order_by(desc(Feedback.timestamp)).limit(25).all()
+    return render_template("account.html", title='Account', form=form, feedback=feedback)
 
 
 @app.route('/login', methods=['GET', 'POST'])
